6eecf36ba162eef56e6666687d17ea5278ede425
ecDusty <ecmgit@outlook.com>
2016-07-05 15:26:31 +0800

    FEAT Added Reverse, Clear and Date to notes
    FEAT Added Reverse, Clear and Date to notes
    
    Reversed the order of the notes that are displayed on the page. Any new
    note is added to the beginning of the list.
    
    Clear Button added after the <ul> on the page, which clears out all
    notes within localStorage
    
    Added a date value to the notes object. This currently only shows the
    Date.now() value when the note is created.
    
    Things still to do:
    - We need to convert the date value into a human readable date

--- js/retain.js
+++ js/retain.js
@@ -1,63 +1,84 @@
 $(function(){
 
-    var model = {
-        init: function() {
-            if (!localStorage.notes) {
-                localStorage.notes = JSON.stringify([]);
-            }
-        },
-        add: function(obj) {
-            var data = JSON.parse(localStorage.notes);
-            data.push(obj);
-            localStorage.notes = JSON.stringify(data);
-        },
-        getAllNotes: function() {
-            return JSON.parse(localStorage.notes);
-        }
-    };
+	var model = {
+		init: function() {
+			if (!localStorage.notes) {
+				localStorage.notes = JSON.stringify([]);
+			}
+		},
+
+		add: function(obj) {
+			var data = JSON.parse(localStorage.notes);
+			data.push(obj);
+			localStorage.notes = JSON.stringify(data);
+		},
+
+		getAllNotes: function() {
+			return JSON.parse(localStorage.notes);
+		},
+
+		clearAllNotes: function() {
+			localStorage.clear();
+			model.init();
+		}
+	};
 
 
-    var octopus = {
-        addNewNote: function(noteStr) {
-            model.add({
-                content: noteStr
-            });
-            view.render();
-        },
+	var octopus = {
+		addNewNote: function(noteStr) {
+			model.add({
+				content: noteStr,
+				date: Date.now()
+			});
+			view.render();
+		},
 
-        getNotes: function() {
-            return model.getAllNotes();
-        },
+		getNotes: function() {
+			return model.getAllNotes().reverse();
+		},
 
-        init: function() {
-            model.init();
-            view.init();
-        }
-    };
+		clearNotes: function() {
+			model.clearAllNotes();
+			view.render();
+		},
+
+		init: function() {
+			model.init();
+			view.init();
+		}
+	};
 
 
-    var view = {
-        init: function() {
-            this.noteList = $('#notes');
-            var newNoteForm = $('#new-note-form');
-            var newNoteContent = $('#new-note-content');
-            newNoteForm.submit(function(e){
-                octopus.addNewNote(newNoteContent.val());
-                newNoteContent.val('');
-                e.preventDefault();
-            });
-            view.render();
-        },
-        render: function(){
-            var htmlStr = '';
-            octopus.getNotes().forEach(function(note){
-                htmlStr += '<li class="note">'+
-                        note.content +
-                    '</li>';
-            });
-            this.noteList.html( htmlStr );
-        }
-    };
+	var view = {
+		init: function() {
+			this.noteList = $('#notes');
+			var newNoteForm = $('#new-note-form');
+			var newNoteContent = $('#new-note-content');
+			var clearButton = $('#clear-button');
 
-    octopus.init();
+			newNoteForm.submit(function(e){
+				octopus.addNewNote(newNoteContent.val());
+				newNoteContent.val('');
+				e.preventDefault();
+			});
+
+			clearButton.click(function() {
+			  octopus.clearNotes();
+			});
+
+			view.render();
+		},
+		render: function(){
+			var htmlStr = '';
+			octopus.getNotes().forEach(function(note){
+				htmlStr += '<li class="note">'+
+						note.content + '<p class="note-date">' +
+						note.date + '</p>'
+					'</li>';
+			});
+			this.noteList.html( htmlStr );
+		}
+	};
+
+	octopus.init();
 });
